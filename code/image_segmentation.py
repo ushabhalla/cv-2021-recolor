800dc4ebde66290c5f7a813465b02966e4c15724
Puneet Bhalla <puneetbhalla@Puneets-MBP.lan>
2021-04-20 15:33:07 -0700

    normalize recoloring

--- code/image_segmentation.py
+++ code/image_segmentation.py
@@ -6,93 +6,74 @@ import argparse
 import random
 
 def parse_args():
+    "Allow user to specify image, k, and output"
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', help='path to data file', default='../data/lotuscopy.jpeg')
-    parser.add_argument('-o', help='path to output directory', default='output')
+    parser.add_argument('-d', help='path to data file', default='../data/irises.jpeg')
+    parser.add_argument('-k', help='number of image cluster from [4,7]', type=int, default=4)
+    parser.add_argument('-o', help='output image file', default='output.jpg')
     return parser.parse_args()
 
 
-def image_distance(original, compressed):
+def image_difference(original, compressed):
     """
-    Calculates the pixel element-wise (pixel-by-pixel) difference and normalizes
-    the result
-
-    :param original: original image
-    :param compressed: compressed image
-    :return: distance from each pixel in image and clustered image
+    Return the difference between the original image and compressed image
     """
-    return original-compressed
+    dist = original-compressed
+    max_val = np.max(dist)
+    return dist/max_val/2
 
 def naive_recolor(image, clustered_image, indices, new_colors):
-    image = image_distance(image, clustered_image)
+    """
+    Recolor the image using specified new_colors
+    """
+    image = image_difference(image, clustered_image)
     indices = np.reshape(indices, (image.shape[0], image.shape[1]))
     for i in range(clustered_image.shape[0]):
         for j in range(clustered_image.shape[1]):
             image[i, j] += new_colors[indices[i, j]]
     return image
 
+
+
+def paper_thing:
+    """
+    first init kmeans with white and black cluster centers with init
+
+    """
+    pass
+
+
+
 def main():
     args = parse_args()
-    # Use io to read in the image pixel data
+
     image = io.imread(args.d)
-    # Get the number of rows and columns
-    rows = image.shape[0]
-    cols = image.shape[1]
-    # Normalize all the pixels by dividing all datapoints by 255
     image = image/255
-    
-    #make sure image is 3 dimensions, not 4    
-    dimension = image.shape[-1]
-    print('image dimension is:', dimension)
 
-    if dimension != 3:
-        if (dimension == 4):
-            image = image[:,:,:3]
-        else:
-            print('image is not valid')
-
-    # Reshape the image so that the data points have 3 attributes, corresponding to RGB values (for each pixel)
     X = image.reshape(image.shape[0]*image.shape[1], 3)
+    K = args.k
 
-    # Number of clusters
-    K = 4
-    # Maximum number of times KMeans should run
-    max_iters = 20
 
-    # Create an instance of the KMeans class
-    kmeans = KMeans(n_clusters=K, max_iter=max_iters).fit(X)
-    # Run the instance of the KMeans class on the data X, with K clusters and max_iters
-    print("Running k-means")
+    print("Clustering")
+    kmeans = KMeans(n_clusters=K, max_iter=20)
+    kmeans.fit(X)
+
     centroids = kmeans.cluster_centers_
     idx = kmeans.labels_
-    
-    if not os.path.exists(args.o):
-        os.makedirs(args.o)
-
-    with open(args.o + "/centroids.txt", 'w+') as f:
-        for c in centroids:
-            f.write("%s\n" % c)
-
-    # with open(args.o + "/indices.txt", 'w+') as f:
-    #     for i in idx:
-    #         f.write("%s\n" % i)
-
-    # Get the compressed version of data X
     X_recovered = centroids[idx]
+    X_recovered = np.reshape(X_recovered, (image.shape[0], image.shape[1], 3))
 
-    # print(f"Image Distance: {image_distance(X, X_recovered)}")
-
-    # Reshape X_recovered into a 1-dimensional np array in order for io to display
-    X_recovered = np.reshape(X_recovered, (rows, cols, 3))
-    new_colors = [[.937, .258, .96], [.25, .96, .25], [.3, .4, .5], [.12, .23, .45]]
+    print("Recoloring")
+    new_colors = [[186, 199, 219], [116, 96, 150], [96, 138, 55], [40, 69, 11]]
+    new_colors = np.array(new_colors)/255
     X_recovered = naive_recolor(image, X_recovered, idx, new_colors)
+    X_recovered = np.clip(X_recovered, 0, 1)
 
     io.imshow(X_recovered)
     io.show()
 
-    # Save the compressed version of the original image
-    io.imsave(args.o + "/output2.jpg", img_as_ubyte(X_recovered))
-    print("Saved compressed image")
+    print("Output saved to output/" + args.o)
+    io.imsave("output/" + args.o, img_as_ubyte(X_recovered))
 
 
 if __name__ == '__main__':
