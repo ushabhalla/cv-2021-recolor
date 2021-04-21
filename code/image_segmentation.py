93f2706481c7812eb1cd46e36c954a48bda1b9e2
sierrarowley <60280017+sierrarowley@users.noreply.github.com>
2021-04-20 23:28:02 -0400

    failed attempt at luminance transfer

--- code/image_segmentation.py
+++ code/image_segmentation.py
@@ -39,12 +39,9 @@ def naive_recolor(image, clustered_image, indices, new_colors):
             image[i, j] += new_colors[indices[i, j]]
     return image
 
-
-def assign_new_colors_by_luminance(centroids, new_colors):
-    new_colors = np.divide(new_colors, 255)
+def sort_luminance(centroids, new_colors):
     centroid_luminance = []
     new_luminance = []
-    new_colors_sorted = np.zeros((new_colors.shape[0], new_colors.shape[1]))
     # calculate luminance
     for i in range(centroids.shape[0]):
         centroid_luminance.append(
@@ -53,13 +50,33 @@ def assign_new_colors_by_luminance(centroids, new_colors):
             0.2126*new_colors[i, 0] + 0.7152*new_colors[i, 1] + 0.0722*new_colors[i, 2])
     centroid_indices = np.argsort(centroid_luminance)
     new_color_indices = np.argsort(new_luminance)
+    return centroid_indices, new_color_indices
+
+def assign_new_colors_by_luminance(centroids, new_colors):
+    new_colors = np.divide(new_colors, 255)
+    new_colors_sorted = np.zeros((new_colors.shape[0], new_colors.shape[1]))
+    centroid_indices, new_color_indices = sort_luminance(centroids, new_colors)
     # reassign indices of new_color to match centroids
     for index in new_color_indices:
         new_colors_sorted[centroid_indices[index],
                           :] = new_colors[new_color_indices[index], :]
     return new_colors_sorted
 
+# def luminance_transfer(centroids, new_colors):
+#     centroid_indices, new_color_indices = sort_luminance(centroids, new_colors)
+#     for color in new_colors:
+
+# def solve_weights(centroids):
+#     all_dists = []
+#     for i in range(centroids.shape[0]):
+#         for j in range(i, centroids.shape[0]):
+#             all_dists.append(centroids[i] - centroids[j])
+#     sigma_r = np.mean(all_dists)
+
+
 def naive_palette(centroids, new_color):
+    """ Currently this just tints the image by whatever new_color is
+    """
     new_color = np.divide(new_color, 255)
     index = 0
     new_colors = np.zeros((centroids.shape[0], centroids.shape[1]))
@@ -79,6 +96,7 @@ def naive_palette(centroids, new_color):
         else:
             # clip colors to remain in gamut
             new_colors[i] = np.clip(centroids[i] + diff, 0, 1)
+            # new_colors[i] = centroids[i]
     return new_colors
 
 def cluster(k, image):
@@ -103,13 +121,13 @@ def run_clustering(k, image):
 
     print("Recoloring")
     # let user pick all colors
-    new_colors = [[186, 199, 219], [116, 96, 150],
-                  [209, 145, 82], [220, 232, 209]]
-    new_colors = assign_new_colors_by_luminance(centroids, new_colors)
+    # new_colors = [[186, 199, 219], [116, 96, 150],
+    #               [209, 145, 82], [220, 232, 209]]
+    # new_colors = assign_new_colors_by_luminance(centroids, new_colors)
 
     # naive palette picker given one color
-    # new_color = [247, 146, 242]
-    # new_colors = naive_palette(centroids, new_color)
+    new_color = [247, 146, 242]
+    new_colors = naive_palette(centroids, new_color)
 
     # recolor image
     X_recovered = naive_recolor(image, X_recovered, idx, new_colors)
